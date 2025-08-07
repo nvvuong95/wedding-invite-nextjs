895316577c8a7728a68edf3caa3cc31968e9f3d3
vuongnguyen <ngvanvuong95@gmail.com>
2025-08-07 14:42:48 +0700

    Chhange backdrop

--- src/sections/wedding-day-section/wedding-day-section.tsx
+++ src/sections/wedding-day-section/wedding-day-section.tsx
@@ -115,7 +115,7 @@ export const WeddingDaySection = ({ lang = "vi" }: WeddingDaySectionProps) => {
                     left: 0,
                     width: "100%",
                     height: "100%",
-                    background: "rgba(0,0,0,0.25)", // Backdrop for the whole image area
+                    background: "rgba(0,0,0,0.5)", // Backdrop for the whole image area
                   }}
                 ></Box>
                 <Box
