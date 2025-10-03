f4b5842c38f8ceb7118c6d0896d32132dc5b5d3e
vuongnguyen <ngvanvuong95@gmail.com>
2025-10-03 13:23:20 +0700

    Fix bug

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,7 @@ import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import WeddingInviteScripts from "@/components/WeddingInviteScript";
 import { LightboxProvider } from "@/context/lightbox-context";
-import { Metadata } from "next";
+import type { Metadata } from "next";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -41,27 +41,25 @@ export const metadata: Metadata = {
 
 export default function RootLayout({
   children,
-}: Readonly<{
+}: {
   children: React.ReactNode;
-}>) {
+}) {
   return (
     <html lang="en">
       <head>
         <link rel="stylesheet" href="/css/style_ladi.css" />
-        {/* <link rel="stylesheet" href="/css/style_animation.css" /> */}
         <link rel="stylesheet" href="/css/style_page.css" />
         <link rel="stylesheet" href="/css/style_element.css" />
         <link rel="stylesheet" href="/css/style_lazyload.css" />
         <meta charSet="UTF-8" />
-        <meta httpEquiv="Cache-Control" content="no-cache" />
         <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
+        <meta httpEquiv="Cache-Control" content="no-cache" />
         <meta httpEquiv="Expires" content="-1" />
         <meta name="keywords" content="" />
         <script
-          type="text/javascript"
           dangerouslySetInnerHTML={{
             __html: `
-              window.ladi_viewport=function(b){var a=document;b=b?b:'innerWidth';var c=window[b];var d=c<768;if(typeof window.ladi_is_desktop=="undefined"||window.ladi_is_desktop==undefined){window.ladi_is_desktop=!d;}var e=960;var f=420;var g='';if(!d){g="width="+e+",user-scalable=no,initial-scale=1.0";}else{var h=1;var i=f;if(i!=c){h=c / i;}g="width="+i+",user-scalable=no,initial-scale="+h+",minimum-scale="+h+",maximum-scale="+h;}var j=a.getElementById("viewport");if(!j){j=a.createElement("meta");j.id="viewport";j.name="viewport";a.head.appendChild(j);}j.setAttribute("content",g);};window.ladi_viewport();window.ladi_fbq_data = [];window.ladi_fbq = function () {window.ladi_fbq_data.push(arguments);};window.ladi_ttq_data = [];window.ladi_ttq = function () {window.ladi_ttq_data.push(arguments);};
+              window.ladi_viewport=function(b){var a=document;b=b?b:'innerWidth';var c=window[b];var d=c<768;if(typeof window.ladi_is_desktop=="undefined"||window.ladi_is_desktop==undefined){window.ladi_is_desktop=!d;}var e=960;var f=420;var g='';if(!d){g="width="+e+",user-scalable=no,initial-scale=1.0";}else{var h=1;var i=f;if(i!=c){h=c / i;}g="width="+i+",user-scalable=no,initial-scale="+h+",minimum-scale="+h+",maximum-scale="+h;}var j=a.getElementById("viewport");if(!j){j=a.createElement("meta");j.id="viewport";j.name="viewport";a.head.appendChild(j);}j.setAttribute("content",g);};window.ladi_viewport();
             `,
           }}
         />
@@ -79,16 +77,6 @@ export default function RootLayout({
           crossOrigin="anonymous"
         />
         <link
-          rel="preload"
-          href="https://fonts.googleapis.com/css2?family=Prata&family=Quicksand:wght@400;700&family=Muli:wght@400;700&family=Play:wght@400;700&family=Dancing+Script:wght@400;700&family=Niramit:wght@400;700&family=Montserrat:wght@400;700&family=Mulish:wght@400;700&family=Itim&family=Oswald:wght@400;700&family=Athiti:wght@400;700&family=Chonburi&family=Philosopher:wght@400;700&family=Roboto+Slab:wght@400;700&display=swap"
-          as="style"
-        />
-        <link
-          href="https://fonts.googleapis.com/css2?family=Prata&family=Quicksand:wght@400;700&family=Muli:wght@400;700&family=Play:wght@400;700&family=Dancing+Script:wght@400;700&family=Niramit:wght@400;700&family=Montserrat:wght@400;700&family=Mulish:wght@400;700&family=Itim&family=Oswald:wght@400;700&family=Athiti:wght@400;700&family=Chonburi&family=Philosopher:wght@400;700&family=Roboto+Slab:wght@400;700&display=swap"
-          rel="stylesheet"
-          type="text/css"
-        ></link>
-        <link
           rel="stylesheet"
           href="/css/wedding_animation.css"
           precedence="default"
@@ -96,8 +84,8 @@ export default function RootLayout({
       </head>
       <body className={`lazyload ${geistSans.variable} ${geistMono.variable}`}>
         <LightboxProvider>{children}</LightboxProvider>
+        <WeddingInviteScripts />
       </body>
-      <WeddingInviteScripts />
     </html>
   );
 }
